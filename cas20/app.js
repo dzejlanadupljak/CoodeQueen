b8bd39cb3e549b7e3cb3af198823c21aecedaa28
Džejlana Dupljak <dzejlanadupljak11@email.com>
2025-07-06 13:04:53 +0200

    metode objekata

--- cas20/app.js
+++ cas20/app.js
@@ -1,54 +1,54 @@
-const osoba={
-ime:"dzejlana",
-prezime:"dupljak",
-godine:22,
-visina:174,
-adresa:{
-    ulica:"jaklja",
-    grad:"np"
-},
-"user name":"dzejlana.dupljak",
-hobiji:["plivanje","programiranje","pevanje"]
-}
+// const osoba={
+// ime:"dzejlana",
+// prezime:"dupljak",
+// godine:22,
+// visina:174,
+// adresa:{
+//     ulica:"jaklja",
+//     grad:"np"
+// },
+// "user name":"dzejlana.dupljak",
+// hobiji:["plivanje","programiranje","pevanje"]
+// }
 
-console.log(osoba.prezime)
+// console.log(osoba.prezime)
 
-console.log(osoba.adresa.ulica)
+// console.log(osoba.adresa.ulica)
 
-console.log(osoba.hobiji[1])
-console.log(osoba["user name"])
-
-
-osoba.zanimanje="uciteljica";
-
-console.log(osoba.zanimanje)
-
-delete osoba.prezime
-console.log(osoba.prezime)
+// console.log(osoba.hobiji[1])
+// console.log(osoba["user name"])
 
 
-for(let i=0;i<osoba.hobiji.length;i++){
-    console.log(osoba.hobiji[i])
+// osoba.zanimanje="uciteljica";
 
-}
+// console.log(osoba.zanimanje)
 
-///1.zadatak
+// delete osoba.prezime
+// console.log(osoba.prezime)
 
-const kljucevi=Object.keys(osoba)
 
-console.log(kljucevi)
-kljucevi.forEach((el, index)=>{
-    console.log(`${index} ima vrednost ${el}`)
+// for(let i=0;i<osoba.hobiji.length;i++){
+//     console.log(osoba.hobiji.length)
 
-}
-)
-//2.zadatak
-let zbir = 0
+// }
 
-kljucevi.forEach((el)=>{
-    if( typeof osoba[el] === "number")
-        zbir += osoba[el]
+// ///1.zadatak
+
+// const kljucevi=Object.keys(osoba)
+
+// console.log(kljucevi)
+// kljucevi.forEach((el, index)=>{
+//     console.log(`${index} ima vrednost ${el}`)
+
+// }
+// )
+// //2.zadatak
+// let zbir = 0
+
+// kljucevi.forEach((el)=>{
+//     if( typeof osoba[el] === "number")
+//         zbir += osoba[el]
     
-}
-)
-console.log("zbir numerickih vrednosti je:" +zbir)
+// }
+// )
+// console.log("zbir numerickih vrednosti je:" +zbir)
